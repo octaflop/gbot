947b5bd6343b620b9c112332aecaa186b804ae3c
Faris Chebib <faris@theluckybead.com>
2018-05-10 10:09:12 -0400

    Add list issues

--- create_issue.py
+++ create_issue.py
@@ -5,16 +5,18 @@ from gidgethub.aiohttp import GitHubAPI
 
 
 async def main():
-    # user = 'octaflop'
-    # repo = 'gbot'
+    user = 'octaflop'
+    repo = 'gbot'
     # issue_id = 1
-    user = 'mariatta'
-    repo = 'strange-relationship'
-    issue_id = 80
+    # user = 'mariatta'
+    # repo = 'strange-relationship'
+    # issue_id = 80
     # await add_issue(user, repo)
     # await comment_on_issue(user, repo, issue_id)
     # await react_issue(user, repo, issue_id)
-    await close_issue(user, repo, issue_id)
+    # await close_issue(user, repo, issue_id)
+    issues = await get_issues(user, repo)
+    print(issues)
 
 
 async def add_issue(user, repo):
@@ -50,9 +52,18 @@ async def close_issue(user, repo, issue_id):
     async with aiohttp.ClientSession() as session:
         gh = GitHubAPI(session, "octaflop", oauth_token=os.getenv("GH_AUTH"))
         await gh.patch(f'/repos/{user}/{repo}/issues/{issue_id}',
-                      data={
-                          'state': 'closed',
-                      })
+                       data={
+                           'state': 'closed',
+                       })
+
+
+async def get_issues(user, repo):
+    async with aiohttp.ClientSession() as session:
+        gh = GitHubAPI(session, "octaflop", oauth_token=os.getenv("GH_AUTH"))
+        url = f'/repos/{user}/{repo}/issues'
+        print(url)
+        issues = gh.getiter(url)
+        return [i async for i in issues]
 
 
 loop = asyncio.get_event_loop()
